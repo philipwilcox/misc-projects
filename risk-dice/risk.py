6dfc61a0fb1059252444afee31a7b3c40d6827d1
Philip Wilcox <philipwilcox@gmail.com>
2016-06-22 22:26:33 -0700

    lint and python 3 updates

--- risk-dice/risk.py
+++ risk-dice/risk.py
@@ -1,29 +1,31 @@
 import random
 import sys
 
-FACES=[1,2,3,4,5,6]
+FACES = [1, 2, 3, 4, 5, 6]
 
-SHOW_ROLLS=True
-DEBUG=False
+SHOW_ROLLS = True
+DEBUG = False
+
 
 def roll(attack_dice, defend_dice):
     """Input: number of dice the attacker rolls, number of dice the defender rolls
 
     Output: tuple, (number of dead attacking armies, number of dead defender armies)"""
-    attack_rolls = sorted([random.choice(FACES) for i in xrange(attack_dice)], reverse=True)
-    defend_rolls = sorted([random.choice(FACES) for i in xrange(defend_dice)], reverse=True)
+    attack_rolls = sorted([random.choice(FACES) for i in range(attack_dice)], reverse=True)
+    defend_rolls = sorted([random.choice(FACES) for i in range(defend_dice)], reverse=True)
     if SHOW_ROLLS:
-        print "Attacker rolls {}, defender rolls {}".format(attack_rolls, defend_rolls)
+        print("Attacker rolls {}, defender rolls {}".format(attack_rolls, defend_rolls))
     attacker_deaths = 0
     defender_deaths = 0
-    for i in xrange(len(defend_rolls)):
+    for i in range(len(defend_rolls)):
         if attack_rolls[i] > defend_rolls[i]:
             defender_deaths += 1
         else:
             attacker_deaths += 1
     if DEBUG:
-        print "Attacker deaths, defender deaths: {}".format((attacker_deaths, defender_deaths))
+        print("Attacker deaths, defender deaths: {}".format((attacker_deaths, defender_deaths)))
     return (attacker_deaths, defender_deaths)
+
 
 def attack(attack_armies, defend_armies):
     """Input: number of armies on the attacking country, number of armies on the defending country.
@@ -41,8 +43,9 @@ def attack(attack_armies, defend_armies):
         defend_dice = 2
     deaths = roll(attack_dice, defend_dice)
     if DEBUG:
-        print "after attack: {}".format((attack_armies - deaths[0], defend_armies - deaths[1]))
+        print("after attack: {}".format((attack_armies - deaths[0], defend_armies - deaths[1])))
     return (attack_armies - deaths[0], defend_armies - deaths[1])
+
 
 def attack_as_long_as_possible(attack_armies, defend_armies):
     """Input: number of armies on the attacking country, number of armies on the defending country.
@@ -50,7 +53,7 @@ def attack_as_long_as_possible(attack_armies, defend_armies):
     while attack_armies > 1 and defend_armies > 0:
         attack_armies, defend_armies = attack(attack_armies, defend_armies)
         if DEBUG:
-            print "After round: {}".format((attack_armies, defend_armies))
+            print("After round: {}".format((attack_armies, defend_armies)))
     return (attack_armies, defend_armies)
 
 if __name__ == '__main__':
@@ -58,6 +61,6 @@ if __name__ == '__main__':
     defend_armies = int(sys.argv[2])
     attack_armies, defend_armies = attack_as_long_as_possible(attack_armies, defend_armies)
     if attack_armies > 1:
-        print "Attacker wins! {} armies are still alive!".format(attack_armies)
+        print("Attacker wins! {} armies are still alive!".format(attack_armies))
     else:
-        print "Defender wins! attacker down to {}, {} defender armies are still alive!".format(attack_armies, defend_armies)
+        print("Defender wins! attacker down to {}, {} defender armies are still alive!".format(attack_armies, defend_armies))
